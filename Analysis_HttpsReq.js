34c1b6d12277369e9bb2a0ecd4e42302405c5338
AzirEmperorShurima <tranvantri352@gmail.com>
2024-08-01 06:47:23 +0700

    dd

---
+++ Analysis_HttpsReq.js
@@ -0,0 +1,26 @@
+const https = require('https');
+
+const options = {
+    hostname: 'vlxx.mobi',
+    port: 443,
+    path: '/',
+    method: 'GET',
+    headers: {
+        'User-Agent': 'Node.js'
+    }
+};
+
+const req = https.request(options, (res) => {
+    console.log(`Status Code: ${res.statusCode}`);
+    console.log('Headers:', res.headers);
+
+    res.on('data', (d) => {
+        process.stdout.write(d);
+    });
+});
+
+req.on('error', (e) => {
+    console.error(e);
+});
+
+req.end();
